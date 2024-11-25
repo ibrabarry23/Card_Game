5f065ae04ba214d00e6cb51cf720d37f8571c093
ibrahimatelybarry <ibrabarry014@gmail.com>
2024-11-25 15:48:06 +0100

    add enum and struct in cardGame.h

--- include/CardGame.h
+++ include/CardGame.h
@@ -1,8 +1,18 @@
-//
-// Created by Ibrahima tely Barry on 18/11/24.
-//
+
 
 #ifndef CARDGAME_H
 #define CARDGAME_H
 
 #endif //CARDGAME_H
+typedef enum  {
+	Fiori,
+	Cuori,
+	Picche,
+	Quadri
+} Seme;
+
+typedef struct Carta {
+	Seme seme ;
+	int valore;
+} carta;
+
--- include/vectors.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Ibrahima tely Barry on 18/11/24.
-//
-
-#ifndef VECTORS_H
-#define VECTORS_H
-
-#endif //VECTORS_H
